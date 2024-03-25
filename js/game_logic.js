955f95a0a285bf8aad7b40aa961eebfecaded239
Pavlo <155533645+pavlo768@users.noreply.github.com>
2024-03-25 08:45:32 +0000

    rename js fail and add favicon icon and optimization js and add name game of storage

--- js/game_logic.js
+++ js/game_logic.js
@@ -1,20 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+
     const timerElement = document.querySelector('.count-down');
     let timerInterval;
 
+ // Функція для оновлення відображення таймера на сторінці
+    function updateTimerDisplay(time) {
 
-    // Функція для оновлення відображення таймера
-    const updateTimerDisplay = (time) => {
-        timerElement.textContent = `Time: ${time}s`;
-    };
+    timerElement.textContent = `Time: ${time}s`;
+}
 
+// Функція для запуску таймера гри
+    function startTimer(time) {
 
-// Функція для запуску таймера
-const startTimer = (time) => {
     timerInterval = setInterval(() => {
         time--;
         updateTimerDisplay(time);
+        // Відтворення музики при наближенні до закінчення часу
         if (time === 3) {
             setTimeout(() => {
                 playMusic('music-fone/music-fone-11.wav'); 
@@ -24,11 +25,14 @@ const startTimer = (time) => {
             handleTimeUp(); 
         }
     }, 1000);
-};
-const handleTimeUp = () => {
+}
+
+// Функція для обробки закінчення часу гри
+    function handleTimeUp() {
     clearInterval(timerInterval);
     alert('Time is up!');
-    disableDeck = true; // Оновлення стану disableDeck
+    disableDeck = true;
+    // Затримка перед закриттям всіх відкритих карток
     setTimeout(() => {
         // Закриття всіх відкритих карток
         const cards = document.querySelectorAll('.card');
@@ -38,33 +42,35 @@ const handleTimeUp = () => {
             }
         });
     }, 4000);
-};
-
+}
     let flipsCount = 0;
     let cardOne, cardTwo;
     let disableDeck = false;
     let matched = 0;
+
    // Обробник подій для карток
-const handleCardClick = () => {
+   function handleCardClick() {
     cards.forEach((card) => {
         card.removeEventListener('click', handleCardClick);
     });
-};
+}
+
 // Додавання обробника подій для карток
-const cards = document.querySelectorAll('.card');
-cards.forEach((card) => {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((card) => {
     card.addEventListener('click', handleCardClick);
 });
+
   // Функція для відтворення звуку при фліпі картки з меншою гучністю
-  const playFlipSound = () => {
+  function playFlipSound() {
     const audio = new Audio('music-fone/music-fone-6.wav');
     audio.volume = 0.60; 
     audio.play();
-};
+}
 
 //  функція для обертання картки з відтворенням звуку
-const flipCard = ({ target: clickedCard }) => {
-    if (disableDeck) return; 
+    function flipCard({ target: clickedCard }) {
+    if (disableDeck) return;
     if (cardOne !== clickedCard && !disableDeck) {
         clickedCard.classList.add('flip');
         flipsCount++;
@@ -83,16 +89,20 @@ const flipCard = ({ target: clickedCard }) => {
         const cardTwoImg = cardTwo.querySelector('.back-view img').src;
         matchCards(cardOneImg, cardTwoImg);
     }
-};
-// Функція для перевірки завершення гри. Перевіряє, чи всі пари карток відкриті та відображені.
-const checkGameCompletion = () => {
+}
+
+// Функція для перевірки завершення гри
+function checkGameCompletion() {
     if (matched == 8) {
         clearInterval(timerInterval);
+        disableDeck = true;
+
         const audio = new Audio('music-fone/music-fone-8.wav');
         audio.volume = 0.40;
         audio.oncanplaythrough = () => {
             setTimeout(() => {
                 alert('You Win!');
+                // Затримка перед закриттям карток
                 setTimeout(() => {
                     shuffleCard();
                     matched = 0;
@@ -100,15 +110,15 @@ const checkGameCompletion = () => {
                     cards.forEach((card) => {
                         card.classList.remove('flip');
                     });
-                }, 100);
+                }, 10000); // Затримка  10 секунд перед закриттям карток
             }, 500);
         };
         audio.play();
     }
-};
+}
 
-// Функція для обробки невідповідності пари карток. Відтворює звуковий ефект помилки
-const mismatchCards = () => {
+//  функція для перевірки невідповідності карток
+    function mismatchCards() {
     const audio = new Audio('music-fone/music-fone-9.wav');
     audio.play();
     audio.volume = 0.60;
@@ -118,11 +128,10 @@ const mismatchCards = () => {
         cardOne = cardTwo = '';
         disableDeck = false;
     }, 700);
-};
+}
 
-// Функція для обробки відповідності пари карток. Якщо дві відкриті картки мають однакові зображення, 
-// вони залишаються обернутими, і лічильник відповідних пар збільшується.
-const matchCards = (img1, img2) => {
+//  функція для перевірки відповідності карток
+    function matchCards(img1, img2) {
     if (img1 === img2) {
         matched++;
         checkGameCompletion(); // Перевірка завершення гри
@@ -130,23 +139,23 @@ const matchCards = (img1, img2) => {
         cardTwo.removeEventListener('click', flipCard);
         cardOne = cardTwo = '';
         disableDeck = false;
-        
+
         // Відтворення звуку після вгадування двох карток
         const audio = new Audio('music-fone/music-fone-7.wav');
         audio.volume = 0.40;
         audio.play();
-        
+
         return;
     }
     setTimeout(() => {
         cardOne.classList.add('shake');
         cardTwo.classList.add('shake');
     }, 400);
-    mismatchCards(); 
-};
+    mismatchCards();
+}
 
   // Функція для перемішування карток
-  const shuffleCard = () => {
+    function shuffleCard() {
     matched = 0;
     disableDeck = false;
     cardOne = cardTwo = '';
@@ -159,31 +168,32 @@ const matchCards = (img1, img2) => {
         imgTag.src = `images/img-${arr[i]}.png`;
         card.addEventListener('click', flipCard);
     });
-};
+}
 
-      // Функція для скидання гри
-      const resetGame = () => {
-        flipsCount = 0;
-        const flipsCountElement = document.querySelector('.flips-count');
-        flipsCountElement.textContent = 'Flips: 0';
-        shuffleCard();
+// Функція для скидання гри
+    function resetGame() {
+    flipsCount = 0;
+    const flipsCountElement = document.querySelector('.flips-count');
+    flipsCountElement.textContent = 'Flips: 0';
+    shuffleCard();
         updateTimerDisplay(0);
-    };
-
+    }
+    
 // Функція для оновлення гри
-const refreshGame = () => {
+    function refreshGame() {
     clearInterval(timerInterval);
     resetGame();
     disableDeck = true; // Оновлення стану disableDeck
-};
+}
 
-    const playMusic = (musicPath) => {
-        const audioPlayer = document.getElementById('audio-player');
-        audioPlayer.src = musicPath;
-        audioPlayer.play();
-        audioPlayer.volume = 0.6;
-    };
-    
+// Функція для відтворення музики
+function playMusic(musicPath) {
+    const audioPlayer = document.getElementById('audio-player');
+    audioPlayer.src = musicPath;
+    audioPlayer.play();
+    audioPlayer.volume = 0.6;
+}
+
 // обробник подій на кнопку "Легкий рівень"
 document.querySelector('.easy_level').addEventListener('click', () => {
     clearInterval(timerInterval);
@@ -199,7 +209,7 @@ document.querySelector('.hard_level').addEventListener('click', () => {
     startTimer(61);
     playMusic('music-fone/music-fone-10.mp3');
     });
-    // обробник подій на кнопку "Refresh"
+// обробник подій на кнопку "Refresh"
     const refreshButton = document.querySelector('.refresh-btn'); 
     refreshButton.addEventListener('click', refreshGame);
 });
